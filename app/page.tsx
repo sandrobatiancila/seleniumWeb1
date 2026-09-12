456bc6d6723a44387e0ef76a7b681a9c08816a5f
sandro <lisandro.batiancila@mlhuillier.com>
2026-09-12 17:02:36 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,149 @@
-import Image from "next/image";
+"use client"
 
 export default function Home() {
+  const onPress1 = () => {
+    const tag = document.getElementById("text-result") as unknown as HTMLInputElement;
+    if (tag) {
+      tag.value += "1"
+    }
+  }
+
+  const onPress2 = () => {
+    const tag = document.getElementById("text-result") as unknown as HTMLInputElement;
+    if (tag) {
+      tag.value += "2"
+    }
+  }
+  const onPress3 = () => {
+    const tag = document.getElementById("text-result") as unknown as HTMLInputElement;
+    if (tag) {
+      tag.value += "3"
+    }
+  }
+  const onPress4 = () => {
+    const tag = document.getElementById("text-result") as unknown as HTMLInputElement;
+    if (tag) {
+      tag.value += "4"
+    }
+  }
+  const onPress5 = () => {
+    const tag = document.getElementById("text-result") as unknown as HTMLInputElement;
+    if (tag) {
+      tag.value += "5"
+    }
+  }
+  const onPress6 = () => {
+    const tag = document.getElementById("text-result") as unknown as HTMLInputElement;
+    if (tag) {
+      tag.value += "6"
+    }
+  }
+  const onPress7= () => {
+    const tag = document.getElementById("text-result") as unknown as HTMLInputElement;
+    if (tag) {
+      tag.value += "7"
+    }
+  }
+  const onPress8 = () => {
+    const tag = document.getElementById("text-result") as unknown as HTMLInputElement;
+    if (tag) {
+      tag.value += "8"
+    }
+  }
+  const onPress9 = () => {
+    const tag = document.getElementById("text-result") as unknown as HTMLInputElement;
+    if (tag) {
+      tag.value += "9"
+    }
+  }
+  const onPress0 = () => {
+    const tag = document.getElementById("text-result") as unknown as HTMLInputElement;
+    if (tag) {
+      tag.value += "0"
+    }
+  }
+
+  const onPressDot = () => {
+    const tag = document.getElementById("text-result") as unknown as HTMLInputElement;
+    if (tag) {
+      tag.value += "."
+    }
+  }
+  
+  const onPressSub = () => {}
+  const onPressAdd = () => {}
+  const onPressMult = () => {}
+  const onPressDiv = () => {}
+  const onPressEq = () => {}
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className=" p-5 w-full bg-white">
+      <div className=" w-1/2 justify-self-center">
+      <span className=" text-center self-center justify-self-center text-[30px] text-emerald-700" id="text-welcome">Welcome</span>
+        {/* TEXT INPUT */}
+        <input type="text" placeholder="0.00" className=" w-full p-5 bg-[#333] rounded-sm text-[50px]" id="input-result"/>
+      </div>
+      {/*  1-3 */}
+      <div className=" w-1/2 justify-self-center flex justify-center items-center m-3 p-2 gap-[20px]">
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPress1}>
+          <span className=" text-[50px]">1</span>
+        </button>
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPress2}>
+          <span className=" text-[50px]">2</span>
+        </button>
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPress3}>
+          <span className=" text-[50px]">3</span>
+        </button>
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPressSub}>
+          <span className=" text-[50px]">-</span>
+        </button>
+      </div>
+      {/* 4-6  */}
+      <div className=" w-1/2 justify-self-center flex justify-center items-center m-3 p-2 gap-[20px]">
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPress4}>
+          <span className=" text-[50px]">4</span>
+        </button>
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPress5}>
+          <span className=" text-[50px]">5</span>
+        </button>
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPress6}>
+          <span className=" text-[50px]">6</span>
+        </button>
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPressAdd}>
+          <span className=" text-[50px]">+</span>
+        </button>
+      </div>
+      {/* 6-9 */}
+      <div className=" w-1/2 justify-self-center flex justify-center items-center m-3 p-2 gap-[20px]">
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPress7}>
+          <span className=" text-[50px]">7</span>
+        </button>
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPress8}>
+          <span className=" text-[50px]">8</span>
+        </button>
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPress9}>
+          <span className=" text-[50px]">9</span>
+        </button>
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer"
+          onClick={onPressMult}>
+          <span className=" text-[50px]">*</span>
+        </button>
+      </div>
+      {/* 0-= */}
+      <div className=" w-1/2 justify-self-center flex justify-center items-center m-3 p-2 gap-[20px]">
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPress0}>
+          <span className=" text-[50px]">0</span>
+        </button>
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPressDiv}>
+          <span className=" text-[50px]">/</span>
+        </button>
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPressDot}>
+          <span className=" text-[50px]">.</span>
+        </button>
+        <button className=" w-30 rounded-sm p-5 bg-emerald-700 cursor-pointer" onClick={onPressEq}>
+          <span className=" text-[50px]">=</span>
+        </button>
+      </div>
     </div>
   );
 }
